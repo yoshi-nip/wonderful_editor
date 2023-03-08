2eb3a61ed02bc42899b49c4412debee4c8f40a1c
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-08 21:36:24 +0900

    Actionsエラー 治せるものは直し、対象外ファイルやディレクトリを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_115710) do
-
+ActiveRecord::Schema.define(version: 20_230_308_115_710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
