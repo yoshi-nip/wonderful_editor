7c42c18b63f578b42efbbd3d2d2507bd1f151c70
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-09 17:38:15 +0900

    rubocop -aで修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_308_115_710) do
+ActiveRecord::Schema.define(version: 20_230_309_073_753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "articles_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["articles_id"], name: "index_article_likes_on_articles_id"
+    t.index ["user_id"], name: "index_article_likes_on_user_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -46,5 +55,7 @@ ActiveRecord::Schema.define(version: 20_230_308_115_710) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "article_likes", "articles", column: "articles_id"
+  add_foreign_key "article_likes", "users"
   add_foreign_key "articles", "users"
 end
