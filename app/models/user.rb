21e4c017b0bf1868dedae2e2f6b8ffc90be1fd10
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-09 16:22:10 +0900

    has_manyとbelong_toの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :articles
 end
