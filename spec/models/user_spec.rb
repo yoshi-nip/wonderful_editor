6f40f499f30a29a4324f2d22df69b95ac0525f7f
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-13 16:57:33 +0900

    user_specのリファクタリング(変数からletに変更、いらないコメントアウトを消去)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,17 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   context "全てが指定されているとき" do
-    it "ユーザーが作られる" do
+    let(:user){build(:user)}
+    fit "ユーザーが作られる" do
       # user = User.new(name: "foo",account: "foo", email: "foo@foo.com")
-      user = build(:user)
+      # user = build(:user)
       expect(user).to be_valid
     end
   end
 
   context "emailが空白の時" do
-    it "ユーザー作成に失敗" do
-      # user = User.new(name: "foo", email: "foo@foo.com")
-      user = build(:user, { email: "" })
+    let(:user){build(:user, { email: "" })}
+    fit "ユーザー作成に失敗" do
       expect(user).to be_invalid
       expect(user.errors.errors[0].attribute).to eq :email
       expect(user.errors.errors[0].type).to eq :blank
@@ -21,9 +21,8 @@ RSpec.describe User, type: :model do
   end
 
   context "password空白の時" do
-    it "ユーザー作成に失敗" do
-      # user = User.new(name: "foo", email: "foo@foo.com")
-      user = build(:user, { password: "" })
+    let(:user){build(:user,password: "")}
+    fit "ユーザー作成に失敗" do
       expect(user).to be_invalid
       expect(user.errors.errors[0].attribute).to eq :password
       expect(user.errors.errors[0].type).to eq :blank
@@ -31,9 +30,8 @@ RSpec.describe User, type: :model do
   end
 
   context "nameが空白の時" do
-    it "ユーザー作成に失敗" do
-      # user = User.new(name: "foo", email: "foo@foo.com")
-      user = build(:user, { name: "" })
+    let(:user) {build(:user,name: "" )}
+    fit "ユーザー作成に失敗" do
       expect(user).to be_invalid
       expect(user.errors.errors[0].attribute).to eq :name
       expect(user.errors.errors[0].type).to eq :blank
@@ -41,29 +39,11 @@ RSpec.describe User, type: :model do
   end
 
   context "passwordが8文字以下" do
-    it "ユーザー作成に失敗" do
-      # user = User.new(name: "foo", email: "foo@foo.com")
-      user = build(:user, { password: "bla322r" })
+    let(:user){build(:user,{password: "vjfysl2"})}
+    fit "ユーザー作成に失敗" do
       expect(user).to be_invalid
       expect(user.errors.errors[0].attribute).to eq :password
       expect(user.errors.errors[0].type).to eq :too_short
-      # expect(user.errors.details[:account][0][:error]).to eq :blank
     end
   end
-
-  # context "acoountが存在していない時" do
-  #   it "ユーザーが作られる" do
-  #   end
-  # end
-
-  # context "accountが存在しているとき" do
-  #   before { create(:user,{name: "bar",account: "foo", email: "bar@foo.com"}) }
-  #   it "ユーザー作成に失敗" do
-  #     # User.create!(name: "bar",account: "foo", email: "bar@foo.com")
-  #     # binding.pry
-  #     user = build(:user,{name: "foo",account: "foo", email: "foo@foo.com"})
-  #     expect(user).to be_invalid
-  #     expect(user.errors.details[:account][0][:error]).to eq :taken
-  #   end
-  # end
 end
