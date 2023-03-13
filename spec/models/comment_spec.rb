1e8f5a23129f5adfede12901e221564e25196be9
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-13 17:00:26 +0900

    comment_specのリファクタリング(変数からletに変更、いらないコメントアウトを消去

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,18 +1,16 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   context "全てが指定されているとき" do
-    it "記事が作られる" do
-      comment = build(:comment)
+    let(:comment) {build(:comment)}
+    fit "記事が作られる" do
       expect(comment).to be_valid
     end
   end
 
   context "bodyが空白の時" do
-    it "記事の作成に失敗" do
-      # user = User.new(name: "foo", email: "foo@foo.com")
-      comment = build(:comment, { body: "" })
+    let(:comment) {build(:comment,{body: ""})}
+    fit "記事の作成に失敗" do
       expect(comment).to be_invalid
       expect(comment.errors.errors[0].attribute).to eq :body
       expect(comment.errors.errors[0].type).to eq :blank
