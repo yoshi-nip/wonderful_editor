0981a7007525f21db56c6ffe759759e6133e2c50
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-14 23:27:46 +0900

    現役エンジニアの方の書き方を見てFactoryの設定を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,11 @@ FactoryBot.define do
     # name {"hoge"}
     # account {"hoge"}
     # email {"hoge@hoge.com"}
-    name { Faker::Name.name }
+    # name { Faker::Name.name }
+    # sequence(:email) {|n| "#{n}_#{Faker::Internet.email}" }
+    # password { Faker::Internet.password }
+    name { Faker::Lorem.characters(number: Random.new.rand(1..30)) }
     sequence(:email) {|n| "#{n}_#{Faker::Internet.email}" }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(min_length: 8, max_length: 32, mix_case: true, special_characters: true) }
   end
 end
