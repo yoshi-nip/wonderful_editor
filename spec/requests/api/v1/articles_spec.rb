fff18405614c5ce1e282c2ca461b5a5aa9cf2e27
yoshi <mdmdmdmdmd57@gmail.com>
2023-03-29 11:20:31 +0900

    現役エンジニアの方のコードをもとにリファクタリング

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -34,11 +34,16 @@ RSpec.describe "Article", type: :request do
     context "指定したidのデータが返ってくること(200)" do
       let(:article) { create(:article) }
       let(:article_id) { article.id }
-      it "記事の詳細を取得できる" do
+      fit "記事の詳細を取得できる" do
         p(subject)
         res = JSON.parse(response.body)
         expect(res.length).to eq 5
+        binding.pry
         expect(res.keys).to eq ["id", "title", "body", "updated_at", "user"]
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
         expect(res["user"].keys).to eq ["id", "name", "email"]
         expect(response).to have_http_status(:ok)
       end
