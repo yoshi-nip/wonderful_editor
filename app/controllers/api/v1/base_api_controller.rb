d47d4b933c4ba2ceee7e43ebbef32d5462feeba7
yoshi <mdmdmdmdmd57@gmail.com>
2023-04-03 14:01:33 +0900

    スタブを使用した、createメソッドのrequestテストの実装

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,6 +1,13 @@
 module Api
   module V1
     class BaseApiController < ApplicationController
+
+      def current_user
+        current_user = User.first
+        binding.pry
+        current_user
+      end
+
     end
   end
 end
