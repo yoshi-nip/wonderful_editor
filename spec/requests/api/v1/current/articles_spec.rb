249fd11bb03737eed51c226a80c4c089f9299102
yoshi <mdmdmdmdmd57@gmail.com>
2023-04-27 21:41:32 +0900

    rubocop -a実行

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Current::Articles", type: :request do
-
   describe "GET /api/v1/current/articles" do
     subject { get(api_v1_current_articles_path, headers:) }
 
