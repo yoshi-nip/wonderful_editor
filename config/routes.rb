436a51a1219c1abef582dca405962c70a7a63fc6
yoshi <mdmdmdmdmd57@gmail.com>
2023-04-28 08:04:09 +0900

    フロントの設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,23 @@ Rails.application.routes.draw do
   get "sign_up", to: "home#index"
   get "sign_in", to: "home#index"
   get "articles/new", to: "home#index"
+  get "articles/draft", to: "home#index"
+  get "articles/drafts/:id/edit", to: "home#index"
+  get "articles/:id/edit", to: "home#index"
   get "articles/:id", to: "home#index"
+  get "mypage", to: "home#index"
 
   namespace :api do
     namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth", controllers: {
+        registrations: "api/v1/auth/registrations",
+      }
       namespace :articles do
         resources :drafts, only: [:index, :show]
       end
       namespace :current do
         resources :articles, only: [:index]
       end
-      mount_devise_token_auth_for "User", at: "auth", controllers: {
-        registrations: "api/v1/auth/registrations",
-      }
       resources :articles
     end
   end
