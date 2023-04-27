58131f9527068a71abd172c79bd162a5c9c8c0b2
yoshi <mdmdmdmdmd57@gmail.com>
2023-04-27 20:31:00 +0900

    記事一覧取得のstatusが返ってきてなかったので修正

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :updated_at
+  attributes :id, :title, :body, :updated_at, :status
   # binding.pry
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
