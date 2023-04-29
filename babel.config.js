9539f8e68cdfdaf6965dce04d730bc33504ea15c
yoshi <mdmdmdmdmd57@gmail.com>
2023-04-29 14:06:46 +0900

    本番環境のエラー解消のため足りないパッケージをinstallしbabel.config.jsに追加記述

--- babel.config.js
+++ babel.config.js
@@ -39,6 +39,7 @@ module.exports = function(api) {
     plugins: [
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
+      "@babel/plugin-proposal-class-static-block",
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
