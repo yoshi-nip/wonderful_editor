d456240cd27b8aa9872317722752950c9066e3f2
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-08 13:08:45 +0900

    spec/requests/api/v1/current/配下の不要なコメントアウトを削除

--- spec/requests/api/v1/current/articles_spec.rb
+++ spec/requests/api/v1/current/articles_spec.rb
@@ -7,25 +7,21 @@ RSpec.describe "Api::V1::Current::Articles", type: :request do
     let!(:user) { create(:user) }
     let!(:headers) { user.create_new_auth_token }
     context "記事がpublishedの時" do
-      # before { create_list(:article, 3) }
       let!(:article1) { create(:article, :published, updated_at: 1.days.ago, user:) }
       let!(:article2) { create(:article, :published, updated_at: 2.days.ago, user:) }
       let!(:article3) { create(:article, :published, title: "一番最初", user:) }
       it "記事の一覧を取得できる" do
         p(subject)
         res = JSON.parse(response.body)
-        # res.length = 3
         expect(res.length).to eq 3
         expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
         expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "status", "user"]
         expect(res[0]["user"].keys).to eq ["id", "name", "email"]
-        # expect(res.keys).to eq ["id", "account", "name", "created_at", "updated_at", "email"]
         expect(response).to have_http_status(:ok)
       end
     end
 
     context "記事がdraftの時" do
-      # before { create_list(:article, 3) }
       let!(:article1) { create(:article, :draft, updated_at: 1.days.ago) }
       let!(:article2) { create(:article, :draft, updated_at: 2.days.ago) }
       let!(:article3) { create(:article, :draft, title: "一番最初") }
